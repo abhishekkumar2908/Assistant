a1f3d5e86d1f34ee831edb4037b396fb3ebdc524
abhishekkumar2908 <abhishekkumar29081999@gmail.com>
2023-03-19 03:40:45 +0530

    Added Authentication facility by google

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,13 +1,17 @@
-'client side component'
+"use client";
 import React from 'react'
 import Image from 'next/image'
+import { signIn } from 'next-auth/react'
 
 function Login() {
   return (
     <div className='flex flex-col h-screen items-center bg-[#4c4d49d8]'>
       <Image src="/logo.jpg" alt="Assistant" width={200} height={100}/>
-      <button className="bglogin">Please Login to Assistant</button>
+      <button className="bglogin text-3xl animate-pulse"
+            onClick={()=>signIn('google')
+                }>Please Login to Assistant</button>
     </div>
+
   )
 }
 
